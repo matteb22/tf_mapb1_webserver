cf1a8f0df44ad3a6ae186bcee70504a34511b047
Matteo Bottini <mbottini@contractor.ea.com>
2018-05-09 20:27:44 -0500

    removed prefix

--- main.tf
+++ main.tf
@@ -40,7 +40,6 @@ resource "aws_security_group" "allow_all" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 }
 
