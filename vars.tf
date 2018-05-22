0a56b533e08f40a3200e8c9faa9749213392f80a
Matteo Bottini <mbottini@contractor.ea.com>
2018-05-21 22:07:06 -0500

    Updated Jenkinsfile and vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,9 @@
+variable "aws_key_name" {
+  description = "provide the key that you provide through aws cli command"
+  default     = ""
+}
+
 variable "my_private_key_path" {
   description = "Enter the path to the SSH Private Key to run provisioner."
-  default = ""
+  default = "~/.ssh/id_rsa"
 }
